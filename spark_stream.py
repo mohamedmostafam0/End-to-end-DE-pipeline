b0d51e1e91049350d651cce9f7fb00729aa69417
mohamed <mohamed1401@icloud.com>
2025-03-12 06:32:11 +0200

    v1.0

--- spark_stream.py
+++ spark_stream.py
@@ -1,5 +1,4 @@
 import logging
-
 from cassandra.cluster import Cluster
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import from_json, col
@@ -69,8 +68,8 @@ def create_spark_connection():
     try:
         s_conn = SparkSession.builder \
             .appName('SparkDataStreaming') \
-            .config('spark.jars.packages', "com.datastax.spark:spark-cassandra-connector_2.13:3.4.1,"
-                                           "org.apache.spark:spark-sql-kafka-0-10_2.13:3.4.1") \
+            .config('spark.jars.packages', "com.datastax.spark:spark-cassandra-connector_2.12:3.4.1,"
+                                           "org.apache.spark:spark-sql-kafka-0-10_2.12:3.4.1") \
             .config('spark.cassandra.connection.host', 'localhost') \
             .getOrCreate()
 
